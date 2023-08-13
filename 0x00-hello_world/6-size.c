7b574d2d0aedcead5872049227dd3dd0bb573241
Ustaz07 <shamsutukor@gmail.com>
2023-08-13 16:18:07 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,20 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	int w;
+	long int x;
+	long long int y;
+	char z;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(z));
+	printf("Size of a int: %lu byte(s)\n", (unsigned long) sizeof(w));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(x));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(y));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(f));
 	return (0);
 }
 
