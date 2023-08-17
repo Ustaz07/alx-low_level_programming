aa19434f7d7cfc5fc375b3b2cfa864f1ead3fa04
Ustaz07 <shamsutukor@gmail.com>
2023-08-17 00:58:19 -0700

    some corrections changes

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,9 +21,9 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(' ');
-					if (result < 10)
-						_putchar(' ');
 					if (result < 100)
+						_putchar(' ');
+					if (result < 10)
 						_putchar(' ');
 				}
 
@@ -36,6 +36,11 @@ void print_times_table(int n)
 				{
 					_putchar(' ');
 					_putchar((result / 10) + '0');
+				}
+				else if (j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
 				}
 				_putchar((result % 10) + '0');
 			}
