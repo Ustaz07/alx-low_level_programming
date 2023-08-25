ff6501ecdb26f46f7e90308a72fd8e411619080e
Ustaz07 <shamsutukor@gmail.com>
2023-08-24 23:54:03 -0700

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer.
+ * @n: an integer for  printing.
+ */
+void print_number(int n)
+{
+	int divisor = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	while (n / divisor >= 10)
+		divisor *= 10;
+
+	while (divisor != 0)
+	{
+		_putchar('0' + n / divisor);
+		n %= divisor;
+		divisor /= 10;
+	}
+}
