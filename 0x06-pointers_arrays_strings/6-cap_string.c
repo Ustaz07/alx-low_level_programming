e84b4e4275871a1314b9dc2f9a17ad2bb31c052b
Ustaz07 <shamsutukor@gmail.com>
2023-08-24 22:52:56 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,22 +1,46 @@
 #include "main.h"
-#include <stdio.h>
+
+int check_seperators(char c);
 
 /**
- * main - Capitalize all words.
- *
- * Return: Always 0.
+ * cap_string - Capitalizing words of a string.
+ * @s: An input string
+ * check_seperators: checks for separators
+ * Return: return s
  */
-int main(void)
-{
-	char s[] = "Expect the best. Prepare for the worst.\n"
-                "Capitalize on what comes.\n"
-                "hello world! hello-world 0123456hello world\thello\n"
-                "world.hello world\n";
-	char *p;
 
-	p = cap_string(s);
-	printf("%s", p);
-	printf("%s", s);
-	return (0);
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
+			s[i] -= 32;
+
+		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+			s[i + 1] -= 32;
+		i++;
+	}
+
+	return (s);
 }
 
+/**
+ * check_seperators - Separators which are new line,space etc
+ * @c: an input
+ *
+ * Return: 1 or 0.
+ */
+int check_seperators(char c)
+{
+	int i = 0;
+	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}' };
+	for (; i < 13; i++)
+	{
+		if (c == seperators[i])
+			return (1);
+	}
+	return (0);
+}
