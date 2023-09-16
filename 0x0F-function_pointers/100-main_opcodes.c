c20a25bb63a60bb78f475ab25e6406c61c321da0
Ustaz07 <shamsutukor@gmail.com>
2023-09-15 17:20:52 -0700

    Hacks

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line argument strings.
+ *
+ * Return: 0 on success, 1 for incorrect number of arguments, 2 for negative number_of_bytes.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return 1;
+	}
+
+	int number_of_bytes = atoi(argv[1]);
+
+	if (number_of_bytes < 0)
+	{
+		printf("Error\n");
+		return 2;
+	}
+
+	unsigned char *main_address = (unsigned char *)main;
+	int i;
+
+	for (i = 0; i < number_of_bytes; i++)
+	{
+		printf("%02x", main_address[i]);
+		if (i < number_of_bytes - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+
+	return 0;
+}
+
