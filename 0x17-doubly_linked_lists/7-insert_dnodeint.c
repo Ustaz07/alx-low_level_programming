9c7a065f916ef5d9948d8399cf10ab32207f445a
Ustaz07 <shamsutukor@gmail.com>
2024-01-25 12:58:33 -0800

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,11 +14,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	if (!h)
-		return NULL;
+		return (NULL);
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
-		return NULL;
+		return (NULL);
 
 	new_node->n = n;
 
@@ -29,7 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (*h)
 			(*h)->prev = new_node;
 		*h = new_node;
-		return new_node;
+		return (new_node);
 	}
 
 	while (temp && i < idx - 1)
@@ -41,7 +41,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!temp)
 	{
 		free(new_node);
-		return NULL;
+		return (NULL);
 	}
 
 	new_node->prev = temp;
@@ -50,6 +50,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp->next->prev = new_node;
 	temp->next = new_node;
 
-	return new_node;
+	return (new_node);
 }
-
